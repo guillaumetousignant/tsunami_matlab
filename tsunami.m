9c2a6966931bc16e5bb0950e34123e368900ade3
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-03-24 10:07:20 -0400

    build global K_1

--- tsunami.m
+++ tsunami.m
@@ -55,16 +55,53 @@ K_3 = zeros(P, M); % P x M
 Q_4 = zeros(1, P); % 1 x P
 Q_5 = zeros(1, M); % 1 x M
 
-for k = 1:N_elements
+for e = 1:N_elements
     K_elem = zeros(3, 3);
+    x = [points(1, elements(1, e));
+         points(1, elements(2, e));
+         points(1, elements(3, e))];
+    y = [points(2, elements(1, e));
+         points(2, elements(2, e));
+         points(2, elements(3, e))];
+    %a = [x_2*y_3 - x_3*y_2; 
+    %     x_3*y_1 - x_1*y_3;
+    %     x_1*y_2 - x_2*y_1];
+    b = [y_2 - y_3;
+         y_3 - y_1;
+         y_1 - y_2];
+    c = [x_3 - x_2;
+         x_1 - x_3;
+         x_2 - x_1];
+    h = [points(3, elements(1, e));
+         points(3, elements(2, e));
+         points(3, elements(3, e))];
+
+    Delta_e = 0.5 * det([1, x(1), y(1); 1, x(2), y(2); 1, x(3), y(3)]); % Area of element e
+
+    % Assembling element stiffness matrix
     for i = 1:3
         for j = 1:3
-            K_elem(i, j) = 
+            K_elem(i, j) = (sum(h))/(12 * Delta_e) * (b(i)*b(j) + c(i)*c(j));
+            if i == j
+                K_elem(i, j) = K_elem(i, j) - omega^2/g * Delta_e/6;
+            else
+                K_elem(i, j) = K_elem(i, j) - omega^2/g * Delta_e/2;
+            end
+        end
+    end
+
+    % Assembling global stiffness matrix
+    for i = 1:3
+        for j = 1:3
+            K_1(elements(i, e), elements(j, e)) = K_1(elements(i, e), elements(j, e)) + K_elem(i, j);
         end
     end
 end
 
 K = K_1 - K_3 * (K_2^-1) * (K_3');
+B = Q_4 + K_3*K_2^-1 * Q_5;
+
+% K * eta = B
 
 end
 
