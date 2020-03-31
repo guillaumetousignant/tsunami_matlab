dfa73dadddd9465cea1c2d966db32b6cd2aec029
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-03-31 12:25:30 -0400

    S I M P L I C I T Y

--- write_solution.m
+++ write_solution.m
@@ -1,4 +1,4 @@
-function [] = write_solution(filename, eta, amplitude, omega, points, elements, wall, farfield)
+function [] = write_solution(filename, eta, amplitude, omega)
 %READ_SU2 Summary of this function goes here
 %   Detailed explanation goes here
 fid = fopen(filename,'w');
@@ -7,30 +7,11 @@ if fid == -1
 end
 
 fprintf(fid, 'AMPLITUDE= %d\n', amplitude);
-fprintf(fid, 'OMEGA= %d\n', omega);
-fprintf(fid, 'NDIM= 3\n\n');
+fprintf(fid, 'OMEGA= %d\n\n', omega);
 
-fprintf(fid, 'NPOIN= %d\n', size(points, 2));
-for i = 1:size(points, 2)
-    fprintf(fid, '%- 12.12g %- 12.12g %- 12.12g %- 12.12g %- 12.12g\n', points(1, i), points(2, i), points(3, i), real(eta(i, 1)), imag(eta(i, 1)));
-end
-
-fprintf(fid, '\nNELEM= %d\n', size(elements, 2));
-for i = 1:size(elements, 2)
-    fprintf(fid, '5 %d %d %d\n', elements(1, i), elements(2, i), elements(3, i));
-end
-
-fprintf(fid, 'NMARK= 2\n');
-fprintf(fid, 'MARKER_TAG= wall\n');
-fprintf(fid, 'MARKER_ELEMS= %d\n', size(wall, 2));
-for i = 1:size(wall, 2)
-    fprintf(fid, '3 %d %d\n', wall(1, i), wall(2, i));
-end
-
-fprintf(fid, 'MARKER_TAG= farfield\n');
-fprintf(fid, 'MARKER_ELEMS= %d\n', size(farfield, 2));
-for i = 1:size(farfield, 2)
-    fprintf(fid, '3 %d %d\n', farfield(1, i), farfield(2, i));
+fprintf(fid, 'NPOIN= %d\n', size(eta, 1));
+for i = 1:size(eta, 1)
+    fprintf(fid, '%- 12.12g %- 12.12g\n', real(eta(i, 1)), imag(eta(i, 1)));
 end
 
 fclose(fid);
