181f08a03d9be20e8436d7196d2731881b7989bf
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-04-11 10:54:05 -0400

    better lambda calculation

--- tsunami.m
+++ tsunami.m
@@ -64,11 +64,12 @@ g = 9.81;                           % [m/s^2]   Gravity
 c_water = 1481;                     % [m/s]     Speed of sound
 frequency = omega./(2*pi);          % [1/s]     Frequency of wave
 period = 1./frequency;              % [s]       Period of wave
-lambda = c_water .* period;         % [m]       Wavelength of wave
 [R, I] = max(abs(points(1, :)));    % [m]       Farfield radius
 h = points(3, I);                   % [m]       Farfield depth
 %k = 2*pi./lambda;                   % [1/m]     Wave number 
 k = omega./sqrt(g * h);             %           k used in textbook, see 4.1.11 
+%lambda = c_water .* period;         % [m]       Wavelength of wave
+lambda = 2*pi./k;         % [m]       Wavelength of wave
 N_waves = length(omega);            %           Number of waves input
 
 % Printing parameters
